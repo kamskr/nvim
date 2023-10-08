eb222a234c79d38ab718c072775bd36c7620b20d
kamskry <kamil.sikora@s3connectedhealth.com>
2023-10-08 22:04:38 +0200

    add harpoon

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  harpoon = {
+    loaded = true,
+    path = "/Users/kamils/.local/share/nvim/site/pack/packer/start/harpoon",
+    url = "https://github.com/theprimeagen/harpoon"
+  },
   material = {
     config = { "\27LJ\2\n8\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\25colorscheme material\bcmd\bvim\0" },
     loaded = true,
@@ -89,6 +94,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/kamils/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
+  },
+  playground = {
+    loaded = true,
+    path = "/Users/kamils/.local/share/nvim/site/pack/packer/start/playground",
+    url = "https://github.com/nvim-treesitter/playground"
   },
   ["plenary.nvim"] = {
     loaded = true,
