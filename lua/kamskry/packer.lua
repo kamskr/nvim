e3c090f7359121d9538fba47f100947e21a43344
kamskry <kamil.sikora@s3connectedhealth.com>
2023-10-10 22:12:30 +0200

    change harpoon config

--- lua/kamskry/packer.lua
+++ lua/kamskry/packer.lua
@@ -51,4 +51,12 @@ return require('packer').startup(function(use)
 
   use("github/copilot.vim")
 
+  use {
+      'akinsho/flutter-tools.nvim',
+      requires = {
+          'nvim-lua/plenary.nvim',
+          'stevearc/dressing.nvim', -- optional for vim.ui.select
+      },
+  }
+
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -109,6 +109,16 @@ _G.packer_plugins = {
     path = "/Users/kamils/.local/share/nvim/site/pack/packer/start/copilot.vim",
     url = "https://github.com/github/copilot.vim"
   },
+  ["dressing.nvim"] = {
+    loaded = true,
+    path = "/Users/kamils/.local/share/nvim/site/pack/packer/start/dressing.nvim",
+    url = "https://github.com/stevearc/dressing.nvim"
+  },
+  ["flutter-tools.nvim"] = {
+    loaded = true,
+    path = "/Users/kamils/.local/share/nvim/site/pack/packer/start/flutter-tools.nvim",
+    url = "https://github.com/akinsho/flutter-tools.nvim"
+  },
   ["friendly-snippets"] = {
     loaded = true,
     path = "/Users/kamils/.local/share/nvim/site/pack/packer/start/friendly-snippets",
