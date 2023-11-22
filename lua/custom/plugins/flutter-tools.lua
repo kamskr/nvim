93ea7327a72c79f95dd14e632b8d2979ca467f4e
kamskry <kamil.sikora@s3connectedhealth.com>
2023-11-22 15:53:02 +0100

    improve performance by changing treesitter config

--- lua/custom/plugins/flutter-tools.lua
+++ lua/custom/plugins/flutter-tools.lua
@@ -11,7 +11,7 @@ return {
             widget_guides = {
                 enabled = true,
             },
-            debugger = {     -- integrate with nvim dap + install dart code debugger
+            debugger = {             -- integrate with nvim dap + install dart code debugger
                 enabled = true,
                 run_via_dap = false, -- use dap instead of a plenary job to run flutter apps
             },
