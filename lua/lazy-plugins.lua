cd045770dfe529640bdb0565b57966e99a2c7685
kamskry <kamil.sikora@s3connectedhealth.com>
2023-11-27 19:11:53 +0100

    save changes

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -160,6 +160,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
+      'nvim-treesitter/nvim-treesitter-context',
     },
     build = ':TSUpdate',
   },
