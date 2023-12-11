3d030ff10ee008b9e77057e275b5a3b4649e39ab
kamskr <sikorakamil26@gmail.com>
2023-12-11 19:38:36 +0100

    add chatGPT shortcuts

--- lua/custom/plugins/chatgpt.lua
+++ lua/custom/plugins/chatgpt.lua
@@ -5,6 +5,8 @@ return {
     require("chatgpt").setup({
       api_key_cmd = "op read op://Personal/3ahhhr2psfaojhfadarpkf6ube/credential --no-newline"
     })
+    vim.api.nvim_set_keymap('n', '<leader>oc', [[:ChatGPT<CR>]],
+      { noremap = true, silent = true, desc = "[O]pen [C]hat [G]PT" })
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",
