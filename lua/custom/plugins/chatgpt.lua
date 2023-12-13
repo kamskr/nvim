7ca2b2b1bb2457e6dc2d917b13b034767b789f8f
kamskr <sikorakamil26@gmail.com>
2023-12-13 14:37:03 +0100

    disable chatgpt

--- lua/custom/plugins/chatgpt.lua
+++ lua/custom/plugins/chatgpt.lua
@@ -1,16 +1,16 @@
-return {
-  "jackMort/ChatGPT.nvim",
-  event = "VeryLazy",
-  config = function()
-    require("chatgpt").setup({
-      api_key_cmd = "op read op://Personal/3ahhhr2psfaojhfadarpkf6ube/credential --no-newline"
-    })
-    vim.api.nvim_set_keymap('n', '<leader>oc', [[:ChatGPT<CR>]],
-      { noremap = true, silent = true, desc = "[O]pen [C]hat [G]PT" })
-  end,
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim"
-  }
-}
+-- return {
+--   "jackMort/ChatGPT.nvim",
+--   event = "VeryLazy",
+--   config = function()
+--     require("chatgpt").setup({
+--       api_key_cmd = "op read op://Personal/3ahhhr2psfaojhfadarpkf6ube/credential --no-newline"
+--     })
+--     vim.api.nvim_set_keymap('n', '<leader>oc', [[:ChatGPT<CR>]],
+--       { noremap = true, silent = true, desc = "[O]pen [C]hat [G]PT" })
+--   end,
+--   dependencies = {
+--     "MunifTanjim/nui.nvim",
+--     "nvim-lua/plenary.nvim",
+--     "nvim-telescope/telescope.nvim"
+--   }
+-- }
