4f40a3e6f414c47cfdd51f8d0ddcb128595de4d1
kamskr <sikorakamil26@gmail.com>
2024-01-02 10:51:54 +0100

    add mason required packages

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -74,7 +74,8 @@ local servers = {
   -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
+  ast_grep = {},
+  pylsp = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
