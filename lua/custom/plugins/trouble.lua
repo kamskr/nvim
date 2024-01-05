c3ca5e31df4b290e8e5bcec5aaac945b46af66ed
kamskr <sikorakamil26@gmail.com>
2024-01-05 11:01:02 +0100

    add trouble

---
+++ lua/custom/plugins/trouble.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    -- your custom config
+    require("trouble").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+
+    -- Keybindings
+    vim.keymap.set("n", "<leader>dl", function() require("trouble").toggle("workspace_diagnostics") end)
+    vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+  end
+}
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,7 +10,7 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+-- vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
