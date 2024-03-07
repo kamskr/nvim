6dbe0d8abe9f2771e15ac5a48ab8e7d9ae5103d8
kamils <kamil.sikora@s3connectedhealth.com>
2024-03-07 23:57:17 +0100

    update colors and remove full highlight from flutter

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,6 +1,18 @@
 return {
-  'brenoprata10/nvim-highlight-colors',
+  'uga-rosa/ccc.nvim',
   config = function()
-    require('nvim-highlight-colors').setup()
+    vim.opt.termguicolors = true
+
+    local ccc = require 'ccc'
+    local mapping = ccc.mapping
+
+    ccc.setup {
+      -- Your preferred settings
+      -- Example: enable highlighter
+      highlighter = {
+        auto_enable = true,
+        lsp = true,
+      },
+    }
   end,
 }
