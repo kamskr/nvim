8d9e52105ec8dcd66df0165a82e1fef90487479f
kamils <kamil.sikora@s3connectedhealth.com>
2024-03-30 14:43:56 +0100

    update configuration and update copilot chat

--- lua/custom/plugins/flutter-tools.lua
+++ lua/custom/plugins/flutter-tools.lua
@@ -7,17 +7,40 @@ return {
     },
     config = function()
         require('flutter-tools').setup {
+            decorations = {
+                statusline = {
+                    -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+                    -- this will show the current version of the flutter app from the pubspec.yaml file
+                    app_version = true,
+                    -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+                    -- this will show the currently running device if an application was started with a specific
+                    -- device
+                    device = true,
+                    -- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
+                    -- this will show the currently selected project configuration
+                    project_config = true,
+                }
+            },
             fvm = true,
             widget_guides = {
                 enabled = true,
             },
-            debugger = {     -- integrate with nvim dap + install dart code debugger
+            debugger = {             -- integrate with nvim dap + install dart code debugger
                 enabled = true,
                 run_via_dap = false, -- use dap instead of a plenary job to run flutter apps
+                exception_breakpoints = {
+                    "uncaughted",
+                    -- "raised",
+                },
                 register_configurations = function(_)
                     require('dap').configurations.dart = {}
                     require('dap.ext.vscode').load_launchjs()
                 end,
+            },
+            dev_log = {
+                enabled = true,
+                notify_errors = true, -- if there is an error whilst running then notify the user
+                -- open_cmd = "tabedit", -- command to use to open the log buffer
             },
             lsp = {
                 color = { -- show the derived colours for dart variables
@@ -28,6 +51,14 @@ return {
                     virtual_text = true, -- show the highlight using virtual text
                     virtual_text_str = '■', -- the virtual text character to highlight
                 },
+                settings = {
+                    showTodos = false,
+                    completeFunctionCalls = true,
+                    -- analysisExcludedFolders = { "**/.dart_tool/**", "**/build/**" },
+                    renameFilesWithClasses = "prompt", -- "always"
+                    enableSnippets = true,
+                    updateImportsOnRename = true,      -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
+                }
             },
         }
     end,
