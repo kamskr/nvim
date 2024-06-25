6c83a3794f1683a1c6cbd49ce1b5dd514f2eff37
Kamil Sikora <sikorakamil26@gmail.com>
2024-06-25 09:47:58 +0200

    remove none-ls and update trouble

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,28 +1,37 @@
 return {
   'folke/trouble.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    -- your custom config
-    require('trouble').setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-
-    -- Keybindings
-    vim.keymap.set('n', '<leader>dl', function()
-      require('trouble').toggle 'workspace_diagnostics'
-    end)
-    vim.keymap.set(
-      'n',
-      '<leader>e',
-      '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
-      {
-        desc = 'Open Diagnostic Popup',
-      }
-    )
-    vim.keymap.set('n', 'gR', function()
-      require('trouble').toggle 'lsp_references'
-    end)
-  end,
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = 'Trouble',
+  keys = {
+    {
+      '<leader>dl',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = 'Diagnostics (Trouble)',
+    },
+    {
+      '<leader>xX',
+      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+      desc = 'Buffer Diagnostics (Trouble)',
+    },
+    {
+      '<leader>cs',
+      '<cmd>Trouble symbols toggle focus=false<cr>',
+      desc = 'Symbols (Trouble)',
+    },
+    {
+      '<leader>cl',
+      '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+      desc = 'LSP Definitions / references / ... (Trouble)',
+    },
+    {
+      '<leader>xL',
+      '<cmd>Trouble loclist toggle<cr>',
+      desc = 'Location List (Trouble)',
+    },
+    {
+      '<leader>xQ',
+      '<cmd>Trouble qflist toggle<cr>',
+      desc = 'Quickfix List (Trouble)',
+    },
+  },
 }
